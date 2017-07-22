f2538d5006cbee6392d0f676ac9a972ae59777f3
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-07-22 22:52:34 +0700

    class1

---
+++ taskPythonBasic/rectangle.py
@@ -0,0 +1,14 @@
+class Rectangle :
+	'''This is Rectangle class'''
+	def __init__(self, width, height):
+
+		self.width= width
+		self.height = height
+	
+	def getWidth(self):
+		return self.width
+	def getHeight(self):
+		return self.height
+	def getArea(self):
+		return self.width * self.height
+      
