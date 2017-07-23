34ce2c4ffe51d02abe4ae7cd6a8287ec884759d0
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-07-23 18:36:46 +0700

    check số nguyên tố

--- taskPythonBasic/songuyento.py
+++ taskPythonBasic/songuyento.py
@@ -1,19 +1,17 @@
-# thuật toán chưa tối ưu
 k=0
 b=int(input(">nhap so muon kiem tra:"))
-if b==2:
+if 0<b<3:
 	print("so ban nhap la so nguyen to")
+	exit()
+
 	
-for a in range(2,b):
+for a in range(2,int(b/2)+1):
 	if b%a==0:
-		k+=1
+		k=1
+		break
+
 if k==0:
   print("la so nguyen to")
 else:
   print("ko la so nguyen to")
 
-
-
-
- 
-			
