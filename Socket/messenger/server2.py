246e20a5dcad3e762d29cee6314fd6dc5797bb06
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-12 22:13:47 +0700

    messenger socket

---
+++ Socket/messenger/server2.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+server = socket.socket()
+host = "127.0.0.1"
+port = 8000
+print (host)
+print (port)
+server.bind((host, port))
+
+server.listen(1000)
+print ('waiting.....')
+
+while True:
+	(client, addr) = server.accept()
+	print ("connecting  :!",addr)
+	for x in range(5):
+		data = client.recv(1024).decode()
+		print ('client>',data)
+		r=input("you send:>")
+		client.send(r.encode())	
+		 
