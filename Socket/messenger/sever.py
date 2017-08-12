cf8d7cc222211ce8a59ce12b9f0255d821961432
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-12 19:08:31 +0700

    Create sever.py

---
+++ Socket/messenger/sever.py
@@ -0,0 +1,19 @@
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host ="192.168.1.3"
+port =8000
+s.connect((host,port))
+
+def ts(str):
+   s.send('e'.encode()) 
+   data = ''
+   data = s.recv(1024).decode()
+   print (data)
+
+while 2:
+   r = input('enter')
+   ts(s)
+
+s.close ()
