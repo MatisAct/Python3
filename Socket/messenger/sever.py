986bdf54cf4d4c76a928b0615d4ff04cfa390dbb
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-12 21:14:59 +0700

    phiên bản 1

--- Socket/messenger/sever.py
+++ Socket/messenger/sever.py
@@ -1,18 +1,18 @@
 import socket
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "127.0.0.1"
 port = 8000
 print (host)
 print (port)
-serversocket.bind((host, port))
+server.bind((host, port))
 
-serversocket.listen(5)
+server.listen(5)
 print ('server started and listening')
 while 1:
-    (clientsocket, addr) = serversocket.accept()
-    print ("da ket noi voi :!",addr)
-    data = clientsocket.recv(1024).decode()
-    print (data)
-    r=input("typing:")
-    clientsocket.send(r.encode())
+    (client, addr) = server.accept()
+    print ("connecting  :!",addr)
+    data = client.recv(1024).decode()
+    print ('client>',data)
+    r=input("you send:>")
+    client.send(r.encode())
