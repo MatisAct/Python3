b5912f2243865df0c808b48bd63b5d9dc49fddf1
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-16 20:39:30 +0700

    creat server :v

--- Socket/creatsocketchatclient/server.py
+++ Socket/creatsocketchatclient/server.py
@@ -8,31 +8,30 @@ host = '0.0.0.0'
 port = 2004 
  
 
-sever = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-sever.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+sever = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # khai báo hàm này là bắt buộc
+sever.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # i don't know it T.T
 
-sever.bind((host, port))
+sever.bind((host, port)) # creat sever with ip and port 
 
 sever.listen(5) 
  
-def clientthread(conn):
+def clientthread(conn): # function input and output messenger
 
      while True:
          conn.send(' ') 
 
-         data = conn.recv(1024) 
-         if data=='bye':
-        	 break
+         data = conn.recv(1024) # input data in client
+       
          print data
         
 
-while True:
+while True: # usually true , when client connect !
 
 
 
-    conn, (ip,port) = sever.accept()
-    print ("wellcome to "+ip+" connecting server! ! !")
-    start_new_thread(clientthread,(conn,)) 
+    conn, (ip,port) = sever.accept() # conn: data text, ip port : number exam:128.120...
+    print ("wellcome to "+ip+" connecting server! ! !") # wellcome to client connect : clap clap:
+    start_new_thread(clientthread,(conn,)) # let's go to function
 
  
 conn.close()
