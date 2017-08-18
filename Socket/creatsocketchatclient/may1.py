1588f4f89350f56b92b082aa2371164ae708c45a
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-18 19:25:45 +0700

    Create may1.py

---
+++ Socket/creatsocketchatclient/may1.py
@@ -0,0 +1,20 @@
+
+import socket
+import sys
+
+s = socket.socket()
+host = socket.gethostname() 
+port = 2004
+s.connect((host,port))
+data=[]
+while True:
+    
+
+    
+        
+    
+    sea=raw_input("<--your messenger-->")
+    s.send('Client A:'+sea)
+    data = s.recv(1024).decode()
+    print (data)
+s.close()
